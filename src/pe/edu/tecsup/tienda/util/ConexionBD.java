2161d18d1a9f3d8a28db1d69be23450f5fac56ae
usuario01 <usuario01@gmail.com>
2021-04-15 21:04:46 -0500

    Create pool database connection

--- src/pe/edu/tecsup/tienda/util/ConexionBD.java
+++ src/pe/edu/tecsup/tienda/util/ConexionBD.java
@@ -1,16 +1,21 @@
 package pe.edu.tecsup.tienda.util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConexionBD {
 	public static Connection obtenerConexion() throws SQLException {
 		Connection con = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost/tienda-v02?useSSL=false", "root", "");
-		} catch (ClassNotFoundException e) {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource datasource = (DataSource) envContext.lookup("jdbc/tienda");
+			con = datasource.getConnection();
+		} catch (NamingException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
